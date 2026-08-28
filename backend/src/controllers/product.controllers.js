9fc93e7699e4751b4dc35e0a03fda790abd13501
Sairaj <sairajlolage@gmail.com>
2026-08-29 02:31:34 +0530

    Implimented the product api for custormer and admin both. The admin can able to do CRUD on product. The customer can able to see list of product and single product.

--- backend/src/controllers/product.controllers.js
+++ backend/src/controllers/product.controllers.js
@@ -1,30 +1,110 @@
-const pool = require('../db/db');
+const pool = require("../db/db");
+const { get } = require("../routes/product.routes");
 
 const getProducts = async (req, res) => {
   try {
-    const result = await pool.query('SELECT * FROM products');
+    const result = await pool.query("SELECT * FROM products WHERE is_active = true");
     res.json(result.rows);
   } catch (err) {
-    console.error('Error fetching products:', err.message);
-    res.status(500).json({ error: 'Internal Server Error' });
+    console.error("Error fetching products:", err.message);
+    res.status(500).json({ error: "Internal Server Error" });
   }
-}
+};
 
 const getProductById = async (req, res) => {
   const { id } = req.params;
   try {
-    const result = await pool.query(`SELECT * FROM products WHERE id = $1`, [id]);
+    const result = await pool.query(`SELECT * FROM products WHERE id = $1 AND is_active = true`, [
+      id,
+    ]);
     if (result.rows.length === 0) {
-      return res.status(404).json({ error: 'Product not found' });
+      return res.status(404).json({ error: "Product not found" });
     }
     res.json(result.rows[0]);
   } catch (err) {
-    console.error('Error fetching product by ID:', err.message);
-    res.status(500).json({ error: 'Internal Server Error' });
+    console.error("Error fetching product by ID:", err.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+const createProduct = async (req, res) => {
+  const { name, description, price, discount_percent, stock } = req.body;
+  try {
+    const result = await pool.query(
+      // `
+      // INSERT INTO products (name, description, price, discount_percent, stock, image_url, is_active)
+      // VALUES
+      // ('T-shirt', 'red cotton t-shirt', 500, DEFAULT, 50, 'image.jpg', DEFAULT);`
+
+      `     INSERT INTO products 
+            (name, description, price, discount_percent, stock, image_url, is_active)
+            VALUES 
+            ($1, $2, $3, $4, $5, DEFAULT, DEFAULT) RETURNING *`,
+      [name, description, price, discount_percent ?? 0, stock],
+    );
+    res.status(201).json(result.rows[0]);
+  } catch (err) {
+    console.error("Error creating product:", err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+const updateProduct = async (req, res) => {
+  const { id } = req.params;
+  const { name, description, price, discount_percent, stock, is_active } = req.body;
+  try {
+    const result = await pool.query(
+      `UPDATE products 
+       SET name = $1, description = $2, price = $3, discount_percent = $4, stock = $5, is_active = $6
+       WHERE id = $7 RETURNING *`,
+      [name, description, price, discount_percent ?? 0, stock, is_active, id],
+    );
+    if (result.rows.length === 0) {
+      return res.status(404).json({ error: "Product not found or inactive" });
+    }
+    res.json(result.rows[0]);
+  } catch (err) {
+    console.error("Error updating product:", err);
+    res.status(500).json({ error: "Internal Server Error" });
   }
 }
 
+const statusUpdateProduct = async (req, res) => {
+  const { id } = req.params;
+  const { is_active } = req.body;
+  try {
+    const result = await pool.query(
+      `UPDATE products 
+       SET is_active = $1
+       WHERE id = $2 RETURNING *`,
+      [is_active, id],
+    );
+    if (result.rows.length === 0) {
+      return res.status(404).json({ error: "Product not found" });
+    }
+    res.json(result.rows[0]);
+  } catch (err) {
+    console.error("Error updating product status:", err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
+const getProductsAdmin = async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM products");
+    res.json(result.rows);
+  } catch (err) {
+    console.error("Error fetching products:", err.message);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+};
+
 module.exports = {
   getProducts,
-  getProductById
+  getProductById,
+
+  createProduct,
+  updateProduct,
+  statusUpdateProduct,
+  getProductsAdmin,
 };
--- backend/src/routes/product.routes.js
+++ backend/src/routes/product.routes.js
@@ -1,10 +1,25 @@
-const express = require('express');
+const express = require("express");
 const route = express.Router();
 
-const { getProducts, getProductById } = require('../controllers/product.controllers');
+const {
+  getProducts,
+  getProductById,
+  createProduct,
+  updateProduct,
+  statusUpdateProduct,
+  getProductsAdmin
+} = require("../controllers/product.controllers");
+
+// admin product routes
+route.get("/admin", getProductsAdmin);
+route.post("/", createProduct);
+route.put("/:id", updateProduct);
+route.patch("/:id", statusUpdateProduct);
 
 // cutomer product routes
-route.get('/', getProducts);
-route.get('/:id', getProductById);
+route.get("/", getProducts);
+route.get("/:id", getProductById);
+
+
 
 module.exports = route;
